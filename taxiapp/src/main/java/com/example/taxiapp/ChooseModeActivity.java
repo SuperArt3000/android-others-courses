2208e70ab05ede19796b57c51b7403d76b50b6d7
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-04-27 19:46:10 +0400

    Added solution

---
+++ taxiapp/src/main/java/com/example/taxiapp/ChooseModeActivity.java
@@ -0,0 +1,34 @@
+package com.example.taxiapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ChooseModeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_mode);
+
+    }
+
+    public void goToPassengerSignIn(View view) {
+        startActivity(new Intent(
+                ChooseModeActivity.this,
+                PassengerSignInActivity.class
+        ));
+    }
+
+    public void goToDriverSignIn(View view) {
+        startActivity(new Intent(
+                ChooseModeActivity.this,
+                DriverSignInActivity.class
+        ));
+    }
+}
---
+++ taxiapp/src/main/java/com/example/taxiapp/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.taxiapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(5000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreenActivity.this, ChooseModeActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
